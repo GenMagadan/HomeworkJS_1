028c14e56191bbe361b9960c082c28163c877816
GenMagadan <genchik111@yandex.ru>
2023-10-05 19:41:03 +0300

    Add task_2

--- task.js
+++ task.js
@@ -2,3 +2,8 @@
 // let Celsius = parseInt(prompt('Введите температуру в градусах цельсия'));
 // let Fahrenheit = (9 / 5) * Celsius + 32;
 // console.log(`Температура ${Celsius}C = ${Fahrenheit}F`);
+
+// // task-2
+// let name = prompt('Введите ваше имя');
+// let admin = name;
+// console.log(`Админ сегодня ${admin}`);
